25e5faca4338e79e38758326b73f5806fde02f55
장연식 <sseregit@knowwheresoft.com>
2025-04-29 11:07:36 +0900

    Header를 활용한 구간 설정을 통해 스트리밍 해볼게요

--- http_range/main.go
+++ http_range/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 const uploadDir = "./uploads"
@@ -67,6 +70,52 @@ func main() {
 	})
 
 	http.HandleFunc("/video", func(writer http.ResponseWriter, request *http.Request) {
+		fileName := request.URL.Query().Get("fileName")
+		if fileName == "" {
+			http.Error(writer, "Invalid file name", http.StatusBadRequest)
+		}
+
+		filepath := filepath.Join(uploadDir, fileName)
+		file, err := os.Open(filepath)
+		if err != nil {
+			http.Error(writer, "Invalid file", http.StatusInternalServerError)
+			return
+		}
+
+		defer file.Close()
+
+		rangeHeader := request.Header.Get("Range")
+		if rangeHeader == "" {
+			http.ServeFile(writer, request, filepath)
+			return
+		}
+
+		// Range : byte=313072-621321
+		// byte = <start> - <end>
+		rangePars := strings.Split(strings.TrimPrefix(rangeHeader, "bytes="), "-")
+		start, _ := strconv.ParseInt(rangePars[0], 10, 64)
+
+		stat, _ := file.Stat()
+		fileSize := stat.Size()
+
+		var end int64
+
+		if len(rangePars) > 1 && rangePars[1] != "" {
+			end, _ = strconv.ParseInt(rangePars[1], 10, 64)
+		} else {
+			end = fileSize - 1
+		}
+
+		if end >= fileSize {
+			end = fileSize - 1
+		}
+
+		writer.Header().Set("Content-Type", "video/mp4")
+		writer.Header().Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, fileSize))
+		writer.Header().Set("Accept-Ranges", "bytes")
+
+		file.Seek(start, io.SeekStart)
+		io.CopyN(writer, file, end-start+1)
 
 	})
 }
